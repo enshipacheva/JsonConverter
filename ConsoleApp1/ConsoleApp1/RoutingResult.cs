06ced0fbdf317861506e2310767e7c8cc9d77587
e.n.shipacheva <e.n.shipacheva@urfu.ru>
2022-08-09 19:27:25 +0500

    add properties to JsonObjects

--- ConsoleApp1/ConsoleApp1/RoutingResult.cs
+++ ConsoleApp1/ConsoleApp1/RoutingResult.cs
@@ -9,8 +9,9 @@ namespace JsonObjects
 {
     internal class RoutingResult
     {
-        
+        public JobInfo Job { get; set; }
 
+        public ResultInfo Results { get; set; }
     }
 
     public class JobInfo
@@ -44,6 +45,37 @@ namespace JsonObjects
         public List<Coord[]> Points { get; set; } = new ();
     }
 
+    public class ResultInfo
+    {
+        [JsonProperty("partid")]
+        public decimal Result { get; set; }
+
+        [JsonProperty("route.length.no.contours")]
+        public decimal RouteLengthNoContours { get; set; }
+
+        [JsonProperty ("route.length")]
+        public decimal RouteLength { get; set; }
+
+        [JsonProperty ("count.time")]
+        public string CountTime { get; set; }
+        public List<RouteComponent> Toolpath { get; set; }
+    }
+
+    public class RouteComponent
+    {
+        [JsonProperty("point.id")]
+        public int PointId { get; set; }
+        public string Mode { get; set; }
+
+        [JsonProperty("part.id")]
+        public int? Part_Id { get; set; }
+
+        public int? Id { get; set; }
+
+        public string? PartId { get; set; }
+
+        public Coord[] Point {get;set;}
+    }
 
 
      public record Coord (decimal X, decimal Y, decimal Сurve);  
